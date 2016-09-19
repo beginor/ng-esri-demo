9fc130137cf536dc11a6a8064063150dfb02b5b8
beginor <beginor@qq.com>
2016-09-19 22:27:37 +0800

    update app layout.

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    //moduleId: module.id,
     selector: 'app',
-    templateUrl: 'dist/app.component.html',
-    styleUrls: ['dist/app.component.css']
+    templateUrl: 'dist/app.component.html'
 })
 export class AppComponent {
 
