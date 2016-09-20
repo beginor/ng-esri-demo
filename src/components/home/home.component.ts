6daf8fe1c8226d19971453f6ff26787f8e5704af
beginor <beginor@qq.com>
2016-09-20 23:37:40 +0800

    start use webpack

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home',
-    templateUrl: 'dist/components/home/home.component.html'
+    templateUrl: './home.component.html'
 })
 export class HomeComponent {
 
