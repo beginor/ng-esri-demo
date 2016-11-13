41b5b3c1727c38f9bb449e2eeac55c0ba8367201
beginor <beginor@qq.com>
2016-11-13 16:06:25 +0800

    add moduleId

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
