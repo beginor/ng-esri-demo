6e21348e81b1877c2bf075966d2c0daf37abfc2c
beginor <beginor@qq.com>
2017-03-13 09:53:10 +0800

    remove bower , use js.arcgis.com prebuild version. upgradeto arcgis js api to 4.3

--- dojoConfig.js
+++ dojoConfig.js
@@ -1,16 +1,6 @@
 (function (global) {
     'use strict';
     global.dojoConfig = {
-        async: true,
-        baseUrl: '.',
-        packages: [
-            { name: 'dgrid', location: 'bower_components/dgrid' },
-            { name: 'dijit', location: 'bower_components/dijit' },
-            { name: 'dojo', location: 'bower_components/dojo' },
-            { name: 'dojox', location: 'bower_components/dojox' },
-            { name: 'dstore', location: 'bower_components/dstore' },
-            { name: 'moment', location: 'bower_components/moment' },
-            { name: 'esri', location: 'bower_components/esri' }
-        ]
+        async: true
     };
 })(window);
