362500738cf7f531d53bc0245eafa9e5e193a4b9
beginor <beginor@qq.com>
2017-04-27 21:28:18 +0800

    add animations to config.

--- systemjs.config.js
+++ systemjs.config.js
@@ -13,6 +13,7 @@
       // our app is within the dist folder
       'app': 'src',
       // angular bundles
+      "@angular/animations": "npm:@angular/animations/bundles/animations.umd.js",
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
