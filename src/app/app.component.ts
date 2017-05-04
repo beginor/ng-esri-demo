2c43ec4ca5d5fb8b4515ae3506c00961ada34329
beginor <beginor@qq.com>
2017-05-04 21:14:42 +0800

    add app.component.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
