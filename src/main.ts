0a69f2d5cdcfc3768c540c8fa86660ab1f3f5a50
beginor <beginor@qq.com>
2017-05-23 09:11:25 +0800

    check is local

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if (!isLocal()) {
+    enableProdMode();
+}
+
+function isLocal() {
+    return document.location.href.indexOf('127.0.0.1') > -1
+        || document.location.hash.indexOf('localhost') > -1;
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule)
     .then(val => {
         //console.log('app bootstrap');
