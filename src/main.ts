96c47dce36f554e57c39f6fcad72db61aa4e6a90
beginor <beginor@qq.com>
2018-01-08 15:03:04 +0800

    update use font awesome 5

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import fontawesome from '@fortawesome/fontawesome';
+import fab from '@fortawesome/fontawesome-free-brands';
+import far from '@fortawesome/fontawesome-free-regular';
+import fas from '@fortawesome/fontawesome-free-solid';
+
 if (environment.production) {
     enableProdMode();
 }
+
+fontawesome.library.add(fab, far, fas);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .then(val => {
