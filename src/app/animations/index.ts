5f7c835fb11cefa893512409f60456997efb43ba
beginor <beginor@qq.com>
2018-04-04 20:25:54 +0800

    all attention-speekers animations

--- src/app/animations/index.ts
+++ src/app/animations/index.ts
@@ -2,6 +2,13 @@ export * from './attention-seekers/bounce';
 export * from './attention-seekers/flash';
 export * from './attention-seekers/head-shake';
 export * from './attention-seekers/jello';
+export * from './attention-seekers/pulse';
+export * from './attention-seekers/rubber-band';
+export * from './attention-seekers/shake';
+export * from './attention-seekers/swing';
+export * from './attention-seekers/tada';
+export * from './attention-seekers/wobble';
+
 export * from './fade-in';
 export * from './flip';
 export * from './flip-in-x';
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,21 +1,50 @@
 import { trigger, transition, useAnimation } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
-import { fadeIn } from '../../animations';
+import {
+    bounce, flash, headShake, jello, pulse, rubberBand, shake, swing, tada,
+    wobble
+} from '../../animations';
 
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.scss'],
     animations: [
-        trigger('fadeIn', [transition(':enter', useAnimation(fadeIn))])
+        trigger('animation', [
+            transition('* => bounce', useAnimation(bounce)),
+            transition('* => flash', useAnimation(flash)),
+            transition('* => headShake', useAnimation(headShake)),
+            transition('* => jello', useAnimation(jello)),
+            transition('* => pulse', useAnimation(pulse)),
+            transition('* => rubberBand', useAnimation(rubberBand)),
+            transition('* => shake', useAnimation(shake)),
+            transition('* => swing', useAnimation(swing)),
+            transition('* => tada', useAnimation(tada)),
+            transition('* => wobble', useAnimation(wobble))
+        ])
     ]
 })
 export class AboutComponent implements OnInit {
 
+    public animation = 'bounce';
+
     constructor() { }
 
-    ngOnInit() {
+    public ngOnInit() {
+    }
+
+    public setAnimation($event: Event): void {
+        const target = $event.target as HTMLSelectElement;
+        this.animation = target.value;
+    }
+
+    public replayAnimation(): void {
+        const anim = this.animation;
+        this.animation = '';
+        setTimeout(() => {
+            this.animation = anim;
+        }, 100);
     }
 
 }
