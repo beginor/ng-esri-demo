96766c787edffad46220d2c5ebd83717e7e37fe5
beginor <beginor@qq.com>
2018-05-13 12:19:42 +0800

    update run script

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-docker run --interactive --tty --rm \
-  --name node-dev \
+docker run --interactive --tty --rm --name node-dev \
   --publish 4200:4200 \
   --volume $(pwd):/app \
   --volume $(pwd)/../dev-env/_npmrc:/root/.npmrc \
