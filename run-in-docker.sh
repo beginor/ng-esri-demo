339f722bc807743739fa8c93fc784254a5c68697
beginor <beginor@qq.com>
2018-05-26 13:24:43 +0800

    publish test port.

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker run --interactive --tty --rm --name node-dev \
   --publish 4200:4200 \
+  --publish 9876:9876 \
   --volume $(pwd):/app \
   --volume $(pwd)/../dev-env/_npmrc:/root/.npmrc \
   --volume $(pwd)/../dev-env/_npm:/root/.npm \
