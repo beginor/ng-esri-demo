c8463f5e8f15854d551de7f820de1812d1c4015e
beginor <beginor@qq.com>
2018-08-13 20:40:46 +0800

    format

--- src/app/components/about/about.component.spec.ts
+++ src/app/components/about/about.component.spec.ts
@@ -5,27 +5,27 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AboutComponent } from './about.component';
 
 describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
+    let component: AboutComponent;
+    let fixture: ComponentFixture<AboutComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AboutComponent ],
-      imports: [
-          NoopAnimationsModule,
-          RouterTestingModule
-      ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AboutComponent],
+            imports: [
+                NoopAnimationsModule,
+                RouterTestingModule
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AboutComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
