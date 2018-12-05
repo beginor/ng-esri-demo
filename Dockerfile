18f46570740ec6ec14df8973112aae868eb7a295
beginor <beginor@qq.com>
2018-12-05 17:49:00 +0800

    fix deploy app name

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM nginx:1.15-alpine
 LABEL MAINTAINER="beginor"
 
 COPY default.conf /etc/nginx/conf.d/default.conf
-COPY dist/ng-seed /usr/share/nginx/html/ng-seed
+COPY dist/ng-esri-demo /usr/share/nginx/html/ng-esri-demo
--- build-docker.sh
+++ build-docker.sh
@@ -5,4 +5,4 @@ npm run build
 find dist -name "*.js" -print0 | xargs -0 gzip -k
 find dist -name "*.css" -print0 | xargs -0 gzip -k
 
-docker build --no-cache --rm -t beginor/ng-seed .
+docker build --no-cache --rm -t beginor/ng-esri-demo .
