0077a4347798b3a01c420bcd1f200becc9630a45
beginor <beginor@qq.com>
2019-01-13 20:31:29 +0800

    fix unit test.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { AppCommonModule } from './common/app-common.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -12,7 +13,8 @@ describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
-                RouterTestingModule
+                RouterTestingModule,
+                AppCommonModule
             ],
             declarations: [
                 AppComponent
--- src/app/home/home/home.component.spec.ts
+++ src/app/home/home/home.component.spec.ts
@@ -31,26 +31,6 @@ describe('HomeComponent', () => {
         expect(target).toBeTruthy();
     }));
 
-    it(`should has a message equals 'Hello, Angular !'`, async(() => {
-        expect(target.message).toEqual('Hello, Angular !');
-    }));
-
-    it(`should has count equals 0`, async(() => {
-        expect(target.count).toEqual(0);
-    }));
-
-    it(`should increase count onClick`, async(() => {
-        expect(target.count).toEqual(0);
-        target.onClick();
-        expect(target.count).toBeGreaterThan(0);
-    }));
-
-    it(`should change message onClick`, async(() => {
-        expect(target.message).toEqual('Hello, Angular !');
-        target.onClick();
-        expect(target.message).toContain('You have clicked');
-    }));
-
     it(`should has a button`, async(() => {
         fixture.detectChanges();
         const btn = targetEl.querySelector('button.btn');
