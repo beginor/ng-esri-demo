28da50d40aca815552b6ee2cb0f53770baf7377b
beginor <beginor@qq.com>
2019-05-29 14:12:48 +0800

    updrade to angular-8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,16 @@ import { environment } from '../environments/environment';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', loadChildren: './home/home.module#HomeModule' },
-    { path: 'about', loadChildren: './about/about.module#AboutModule' }
+    {
+        path: 'home',
+        // tslint:disable-next-line: max-line-length
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    },
+    {
+        path: 'about',
+        // tslint:disable-next-line: max-line-length
+        loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+    }
 ];
 
 @NgModule({
