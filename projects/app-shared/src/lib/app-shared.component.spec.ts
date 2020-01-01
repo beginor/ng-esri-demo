062c8cbe28aecd324bd5f4609b8196e373cc9dbf
beginor <beginor@qq.com>
2020-01-01 08:45:13 +0800

    format app shared.

--- projects/app-shared/src/lib/app-shared.component.spec.ts
+++ projects/app-shared/src/lib/app-shared.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppSharedComponent } from './app-shared.component';
 
 describe('AppSharedComponent', () => {
-  let component: AppSharedComponent;
-  let fixture: ComponentFixture<AppSharedComponent>;
+    let component: AppSharedComponent;
+    let fixture: ComponentFixture<AppSharedComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppSharedComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppSharedComponent]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppSharedComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppSharedComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
