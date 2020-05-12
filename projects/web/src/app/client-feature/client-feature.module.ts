e71d0f2c8d901b5d6916fdae44dd88980a4d20de
beginor <beginor@qq.com>
2020-05-12 14:55:28 +0800

    client feature demo.

--- projects/web/src/app/client-feature/client-feature.module.ts
+++ projects/web/src/app/client-feature/client-feature.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ClientFeatureRoutingModule } from './client-feature-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +10,7 @@ import { HomeComponent } from './home/home.component';
     declarations: [HomeComponent],
     imports: [
         CommonModule,
+        FormsModule,
         ClientFeatureRoutingModule
     ]
 })
--- projects/web/src/app/client-feature/home/home.component.ts
+++ projects/web/src/app/client-feature/home/home.component.ts
@@ -1,15 +1,146 @@
 import { Component, OnInit } from '@angular/core';
 
+import { loadModules } from 'esri-loader';
+import * as arcgis from 'esri-service';
+
+import { MapService } from '../../services/map.service';
+
 @Component({
-    selector: 'app-home',
+    selector: 'app-client-feature-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-    constructor() { }
+    public serviceUrl = 'https://it.gdeei.cn/gis/rest/services/wgs84_gd_env_water_section_geo_his_data/MapServer/0'
+    public where = 'business_id = \'69e0118b-7df7-4385-8768-390c53d32ead\' and stat_time = \'2020-03-01\'';
+
+    private layer: __esri.FeatureLayer;
+
+    constructor(
+        private map: MapService
+    ) { }
 
     public ngOnInit(): void {
+    }
+
+    public async addLayer(): Promise<void> {
+        if (!this.serviceUrl) {
+            alert('Invalid serviceUrl');
+            return;
+        }
+        const featureSet = await arcgis.executeQuery(
+            this.serviceUrl,
+            {
+                where: this.where,
+                returnGeometry: true,
+                outFields: [
+                    'objectid',
+                    'section_code',
+                    'section_name',
+                    'area_id',
+                    'longitude',
+                    'latitude',
+                    'area_name',
+                    'tags',
+                    'basin_ids',
+                    'basin_names',
+                    'unit_ids',
+                    'unit_names',
+                    'stat_time',
+                    'business_id',
+                    'is_ok',
+                    'wq_level_id',
+                    'wq_level_name',
+                    'wq_aim_id',
+                    'wq_aim_name',
+                    'wq_index',
+                    'wq_index_avg',
+                    'classify_items',
+                    'water_body_name'
+                ],
+            }
+        );
+        const layer = await arcgis.createFeatureLayer({
+            fields: featureSet.fields,
+            source: featureSet.features,
+            geometryType: 'point',
+            objectIdField: 'objectid',
+            spatialReference: featureSet.spatialReference,
+            renderer: {
+                type: 'simple',
+                symbol: {
+                    type: 'point-3d',
+                    callout: {
+                        size: 1,
+                        type: 'line',
+                        color: '#1AAAA0',
+                        border: {
+                            size: 0.5,
+                            color: '#FFFFFF'
+                        }
+                    },
+                    symbolLayers: [
+                        {
+                            size: 32,
+                            type: 'icon',
+                            anchor: 'bottom',
+                            border: {
+                                size: 1,
+                                color: '#FFFFFF'
+                            },
+                            resource: {
+                                href: 'https://app.gdeei.cn/arcgis-js-api/map-icons/assets/scenes/water-section-nation-1_1.png'
+                            }
+                        }
+                    ],
+                    verticalOffset: {
+                        screenLength: 50,
+                        maxWorldLength: 300,
+                        minWorldLength: 1
+                    }
+                }
+            } as any,
+            labelingInfo: [
+                {
+                    symbol: {
+                        type: 'label-3d',
+                        symbolLayers: [
+                            {
+                                halo: {
+                                    size: 1.5,
+                                    color: [0, 0, 0, 0.6]
+                                },
+                                size: 12,
+                                type: 'text',
+                                material: {
+                                    color: [ 200, 255, 255]
+                                }
+                            }
+                        ]
+                    },
+                    labelPlacement: 'above-center',
+                    useCodedValues: false,
+                    labelExpressionInfo: {
+                        expression: '$feature.section_name'
+                    }
+                }
+            ] as any[],
+            labelsVisible: true
+        });
+        this.map.sceneView.subscribe(view => {
+            view.map.add(layer);
+            this.layer = layer;
+        });
+    }
+
+    public removeLayer(): void {
+        if (!this.layer) {
+            return;
+        }
+        this.map.sceneView.subscribe(view => {
+            view.map.remove(this.layer);
+        });
     }
 
 }
