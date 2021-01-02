f864a4ac794fb1b39dc01befe85a2e734a0d0532
beginor <beginor@qq.com>
2021-01-02 18:19:07 +0800

    update svg icons

--- projects/app-shared/src/lib/svg-icon/svg-icon.component.ts
+++ projects/app-shared/src/lib/svg-icon/svg-icon.component.ts
@@ -6,7 +6,7 @@ import { SvgIconService } from './svg-icon.service';
     // tslint:disable-next-line: component-selector
     selector: 'svg-icon',
     template: `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-app" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11 2H5a3 3 0 0 0-3 3v6a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z"/></svg>`,
-    styles: [``]
+    styles: [`:host { vertical-align: text-bottom; }`]
 })
 export class SvgIconComponent implements AfterViewInit {
 
